2d8bda18c316161880373afb1fc56aff3934b002
ltfide <lutfidendians@gmail.com>
2024-02-19 20:26:07 +0700

    add is false

--- easy/java/Assert.java
+++ easy/java/Assert.java
@@ -1,10 +1,21 @@
 public class Assert {
+
+    private static void print(boolean tOrF) {
+        System.out.println("Expected: " + !tOrF);
+        System.out.println("Result  : " + tOrF);
+    }
     
     public static boolean isTrue(boolean tOrF) {
         if (!tOrF) {
-            System.out.println("Expected: " + true);
-            System.out.println("Result  : " + tOrF);
+            print(tOrF);
         }
         return tOrF;
+    }
+
+    public static boolean isFalse(boolean tOrF) {
+        if (tOrF) {
+            print(tOrF);
+        }
+        return !tOrF;
     }
 }
