40b6ec76eff358b84b0f4e244b33a75cccd105eb
ltfide <lutfidendians@gmail.com>
2025-03-06 20:23:38 +0700

    3. Longest Substring Without Repeating Characters

--- algorithm/java/LongestSubstringWithoutRepeatingChars.java
+++ algorithm/java/LongestSubstringWithoutRepeatingChars.java
@@ -1,7 +1,32 @@
+import java.util.HashMap;
+
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * Diflty : Medium
+ * URL    : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * Date   : 05-03-2025
+ * Author : Lutfi
+ * 
+ * Example 1
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * 
+ * Example 2
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * 
+ * Example 3
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
 public class LongestSubstringWithoutRepeatingChars {
 
   static int lengthOfLongestSubstring(String s) {
-    HashMap<Charachter, Integer> charIdx = new HashMap<>();
+    HashMap<Character, Integer> charIdx = new HashMap<>();
     int left = 0;
     int maxLength = 0;
 
@@ -18,4 +43,4 @@ public class LongestSubstringWithoutRepeatingChars {
 
     return maxLength;
   }
-}
+}`
