4038dc15576c2bfea51ad70df9f68d10d6128e63
ltfide <lutfidendians@gmail.com>
2025-03-14 23:28:36 +0700

    28. Find the Index of the First Occurrence in a String

--- algorithm/java/FindIndexFirstString.java
+++ algorithm/java/FindIndexFirstString.java
@@ -1,7 +1,7 @@
 /**
  * 28. Find the Index of the First Occurrence in a String
  * Diflty : Easy
- * URL    : https://leetcode.com/problems/remove-duplicates-from-sorted-array
+ * URL    : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
  * Date   : 14-03-2025
  * Author : Lutfi
  */
